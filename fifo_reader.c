6fac7f142895ee4b11807150ee1843cedb33c0d8
Mohamed Ahmed <mohamedstarwars@gmail.com>
2024-01-30 10:33:01 +0200

    Adding fifo_reader

---
+++ fifo_reader.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char * argv[])
+{
+    int fd = open("fifo2",O_RDONLY);
+
+    if (fd == -1)
+    {
+        printf("Error opening file.\r\n");
+    }
+    else
+    {
+        char readChar;
+        for (int i = 0; i < 5; i++)
+        {
+            read(fd,&readChar,sizeof(char));
+            printf("Character read: %c\r\n",readChar);
+        }
+        printf("Finished reading file\r\n");
+    }
+
+    return 0;
+}
